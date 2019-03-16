733492fb1f24f937916d0830de07a601ccfbfde5
yunche <yunche@qq.com>
2019-03-16 20:07:45 +0800

    完成项目广告系统的主体工程搭建和多个Eureka服务端的相互注册

---
+++ README.md
@@ -0,0 +1,4 @@
+# 介绍
+
+本项目参考于慕课网[基于 SpringCloud 微服务架构下  广告系统设计与实现](https://coding.imooc.com/class/chapter/310.html#Anchor)，仅供个人学习微服务架构之用。
+
---
+++ ad-eureka/src/main/java/com/yunche/ad/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.yunche.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @ClassName: EurekaApplication
+ * @Description:
+ * @author: yunche
+ * @date: 2019/03/16
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
